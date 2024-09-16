5417e6c24725a6399dda2bbe98bb8b0bc2e27033
TimMacrae <tim@getarthy.com>
2024-09-16 16:29:51 +0200

    create movies schema, data and job to insert data into db

---
+++ apps/backend/jobs/movies/create-movie-db.job.ts
@@ -0,0 +1,24 @@
+import Movie from "./movie-schema.model";
+import { connectDB } from "../../helper/connectDB.helper";
+import { movies } from "./movies";
+import mongoose from "mongoose";
+
+const insertMovies = async () => {
+  try {
+    await connectDB();
+    for (let movie of movies) {
+      const newMovie = new Movie({
+        ...movie,
+        user_id: new mongoose.Types.ObjectId(),
+      });
+      await newMovie.save();
+      console.log(`Inserted: ${movie.title}`);
+    }
+    mongoose.connection.close();
+  } catch (err) {
+    console.error(err);
+    mongoose.connection.close();
+  }
+};
+
+insertMovies();
