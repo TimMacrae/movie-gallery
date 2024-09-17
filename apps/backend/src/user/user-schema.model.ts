1b1e2ec22c73dd7b96fe23153ec4109a4b8812a9
TimMacrae <tim@getarthy.com>
2024-09-17 15:25:28 +0200

    add new methods to user-schema.model

--- apps/backend/src/user/user-schema.model.ts
+++ apps/backend/src/user/user-schema.model.ts
@@ -1,16 +1,29 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import { IUser } from "./user.type";
 
-interface IUser extends Document {
-  username: string;
-  email: string;
-  password: string;
-}
+const userSchema: Schema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const userSchema: Schema = new Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password as string, salt);
+  next();
 });
+
+// Method to match entered password with hashed password
+userSchema.methods.matchPassword = async function (enteredPassword: string) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model<IUser>("User", userSchema);
 
