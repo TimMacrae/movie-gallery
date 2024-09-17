878ca8326aa3f1ce403b9e94c4eddec436a680a0
TimMacrae <tim@getarthy.com>
2024-09-17 15:24:46 +0200

    update create-user-job

--- apps/backend/jobs/user/create-user-db.job.ts
+++ apps/backend/jobs/user/create-user-db.job.ts
@@ -3,17 +3,15 @@ import { connectDB } from "../../helper/connectDB.helper";
 import { comments } from "../comments/comments";
 import mongoose from "mongoose";
 import { faker } from "@faker-js/faker";
-const bcrypt = require("bcrypt");
 
 const insertUsers = async () => {
   try {
     await connectDB();
     for (let comment of comments) {
-      const salt = await bcrypt.genSalt(10);
       const newUser = new User({
         _id: new mongoose.Types.ObjectId(comment.user_id),
         username: faker.internet.userName(),
-        password: await bcrypt.hash(faker.internet.password(), salt),
+        password: faker.internet.password(),
         email: faker.internet.email(),
       });
       await newUser.save();
