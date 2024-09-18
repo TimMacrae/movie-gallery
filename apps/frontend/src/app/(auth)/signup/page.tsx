606ab9e8c517bf02a5c145e3b2e1f6fd5f5d30a6
TimMacrae <tim@getarthy.com>
2024-09-18 16:44:18 +0200

    add signup components / page /query

--- apps/frontend/src/app/(auth)/signup/page.tsx
+++ apps/frontend/src/app/(auth)/signup/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
+import { SignupForm } from "@/src/components/auth/signup-form.component";
 
 export default function SignupPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen">SINGUP</div>
+    <div className="flex items-center justify-center min-h-screen">
+      <SignupForm />
+    </div>
   );
 }
