e479c1e394a40ed6614dfc308a8049e9c4caf5c8
TimMacrae <tim@getarthy.com>
2024-09-18 16:45:26 +0200

    add useUser query

---
+++ apps/frontend/src/hooks/useUser.query.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { APIROUTES } from "../api/api-routes.config";
+
+axios.defaults.withCredentials = true;
+
+const fetchUser = async () => {
+  const { data } = await axios.get(
+    `${APIROUTES.API.ENDPOINT}${APIROUTES.API.GET_USER}`
+  );
+  return data;
+};
+
+export const useUser = () => {
+  return useQuery({
+    queryKey: [APIROUTES.QUERY_KEYS.USER],
+    queryFn: fetchUser,
+    staleTime: 5 * 10000,
+  });
+};
