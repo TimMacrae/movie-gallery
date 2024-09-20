25ff9d1a8569ea2019957e36af1d09ad69d1d693
TimMacrae <tim@getarthy.com>
2024-09-20 13:06:17 +0200

    update movie item

--- apps/frontend/src/components/movies/movie-item.component.tsx
+++ apps/frontend/src/components/movies/movie-item.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { Movie } from "./types/movie.type";
+import { Movie } from "../../types/movie.type";
 import React from "react";
 import { Star } from "lucide-react";
 
@@ -9,21 +9,18 @@ interface MovieItemProps {
 }
 
 export const MovieItem: React.FC<MovieItemProps> = ({ movie }) => {
-  const releaseYear = new Date(movie.release_date).getFullYear();
+  const { release_date, poster, title, genre, averageRating } = movie;
+  const releaseYear = new Date(release_date).getFullYear();
+
   return (
     <div className="relative w-full h-full overflow-hidden rounded-lg shadow-lg">
-      <Image
-        src={movie.poster}
-        alt={movie.title}
-        layout="fill"
-        objectFit="cover"
-      />
+      <Image src={poster} alt={title} layout="fill" objectFit="cover" />
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-4">
-        <h1 className="text-white text-xl font-bold">{movie.title}</h1>
-        <p className="text-white">{movie.genre}</p>
+        <h1 className="text-white text-xl font-bold">{title}</h1>
+        <p className="text-white">{genre}</p>
         <p className="text-white">{releaseYear}</p>
         <p className="text-white flex items-center">
-          {movie.averageRating}
+          {averageRating}
           <Star size={15} style={{ marginLeft: 4, marginTop: -2 }} />
         </p>
       </div>
