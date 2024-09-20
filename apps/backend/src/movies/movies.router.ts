40e019c25df3aa453d542286decf467537e27c87
TimMacrae <tim@getarthy.com>
2024-09-20 13:05:20 +0200

    update movie filter route

--- apps/backend/src/movies/movies.router.ts
+++ apps/backend/src/movies/movies.router.ts
@@ -1,13 +1,7 @@
 import express, { Request, Response } from "express";
-import User from "../../model/user-schema.model";
-import { generateToken } from "../../helper/generate-token.helper";
-import {
-  SignupRequestBody,
-  SigninRequestBody,
-} from "packages/types/auth/auth.type";
-import cookie, { serialize } from "cookie";
-import { verify } from "jsonwebtoken";
 import Movie from "../../model/movie-schema.model";
+import { yearToDate } from "../../helper/year-to-date.helper";
+import { MoviesQueryParams } from "../../types/movie.type";
 
 const router = express.Router();
 
@@ -15,28 +9,39 @@ router.get("/", async (req: Request, res: Response) => {
   const {
     page = 1,
     limit = 10,
-    search = "",
+    search,
     genre,
     releaseYear,
-    top10,
     sortBy,
     sortOrder = "asc",
-  } = req.query;
-  console.log("🚀 => ", req.query);
+  } = req.query as unknown as MoviesQueryParams;
   try {
     const query: any = {};
 
     // Filtering
     if (search) query.title = { $regex: search, $options: "i" };
     if (genre) query.genre = genre;
-    if (releaseYear) query.releaseYear = releaseYear;
-    if (top10) query.averageRating = { $gte: 8 };
+    if (releaseYear) {
+      const startDate = yearToDate(releaseYear);
+      if (startDate) {
+        const endDate = new Date(startDate);
+        endDate.setFullYear(endDate.getFullYear() + 1);
+        query.release_date = { $gte: startDate, $lt: endDate };
+      }
+    }
 
     // Sorting
     const sortOptions: any = {};
-    if (sortBy) {
+    const validSortFields = [
+      "release_date",
+      "averageRating",
+      "ratings",
+      "commentsTotal",
+    ];
+    if (sortBy && validSortFields.includes(sortBy as string)) {
       sortOptions[sortBy as string] = sortOrder === "asc" ? 1 : -1;
     }
+    sortOptions._id = 1;
 
     const movies = await Movie.find(query)
       .sort(sortOptions)
@@ -45,11 +50,21 @@ router.get("/", async (req: Request, res: Response) => {
 
     const totalMovies = await Movie.countDocuments(query);
 
-    res.json({
+    res.status(200).json({
       movies,
       totalPages: Math.ceil(totalMovies / Number(limit)),
       currentPage: Number(page),
     });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/movie/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const movie = await Movie.findById(id);
+    res.status(200).json(movie);
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
