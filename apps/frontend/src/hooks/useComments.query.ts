d10e046cee99e0880cc8396cc464a19a464117aa
TimMacrae <tim@getarthy.com>
2024-09-22 15:53:01 +0200

    add useComments query

---
+++ apps/frontend/src/hooks/useComments.query.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { APIROUTES } from "../api/api-routes.config";
+import { Comment, CommentIds } from "../types/movie.type";
+
+export const getComments = async (
+  commentIds: CommentIds
+): Promise<Comment[]> => {
+  const response = await axios.post<Comment[]>(
+    `${APIROUTES.API.ENDPOINT}${APIROUTES.API.POST_COMMENTS}`,
+    {
+      commentIds,
+    },
+    {
+      withCredentials: true,
+    }
+  );
+  return response.data;
+};
+
+export const useComments = (commentIds: CommentIds) => {
+  return useQuery({
+    queryKey: [APIROUTES.QUERY_KEYS.COMMENTS],
+    queryFn: () => getComments(commentIds),
+  });
+};
