4758fb7265d6b550f78a54fbd0615d492947c9e7
TimMacrae <tim@getarthy.com>
2024-09-22 15:50:32 +0200

    add single movie link

--- apps/frontend/src/app/movies/page.tsx
+++ apps/frontend/src/app/movies/page.tsx
@@ -3,11 +3,12 @@
 import React, { useState } from "react";
 import { useMovies } from "../../hooks/useMovies.query";
 import { MovieItem } from "../../components/movies/movie-item.component";
-// import Link from "next/link";
-// import { APIROUTES } from "@/src/api/api-routes.config";
+import Link from "next/link";
+import { APIROUTES } from "@/src/api/api-routes.config";
 import { MoviesPagination } from "@/src/components/movies/movie-pagination.component";
 import { MoviesFilter } from "@/src/components/movies/movies-filter.component";
 import { MoviesQueryFilter } from "@/src/types/movie.type";
+import { Card } from "@/components/ui/card";
 
 export default function MoviesPage() {
   const [pageNumber, setPageNumber] = useState(1);
@@ -27,22 +28,27 @@ export default function MoviesPage() {
         setMovieFilters={setMovieFilters}
         setPageNumber={setPageNumber}
       />
-      <div
-        className="grid  grid-cols-2 md:grid-cols-3 xl:grid-cols-4 xl:grid-rows-2 gap-9 px-6 py-6 "
-        style={{ height: "82vh" }}
-      >
-        {isLoading ? (
-          <div className="flex items-center justify-center min-h-screen">
-            Loading...
-          </div>
-        ) : (
-          data?.movies.map((movie) => (
-            // <Link key={movie._id} href={`${APIROUTES.API.GET_MOVIE}/${movie._id}`}>
-            <MovieItem key={movie._id} movie={movie} />
-            // </Link>
-          ))
-        )}
-      </div>
+      <Card className="m-4">
+        <div
+          className="grid  grid-cols-2 md:grid-cols-3 xl:grid-cols-4 xl:grid-rows-2 gap-9 px-6 py-6 "
+          style={{ height: "82vh" }}
+        >
+          {isLoading ? (
+            <div className="flex items-center justify-center min-h-screen">
+              Loading...
+            </div>
+          ) : (
+            data?.movies.map((movie) => (
+              <Link
+                key={movie._id}
+                href={`${APIROUTES.API.GET_MOVIES}/${movie._id}`}
+              >
+                <MovieItem key={movie._id} movie={movie} />
+              </Link>
+            ))
+          )}
+        </div>
+      </Card>
       <MoviesPagination
         setPageNumber={setPageNumber}
         pageNumber={data?.currentPage}
