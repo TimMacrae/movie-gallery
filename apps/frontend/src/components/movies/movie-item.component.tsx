1032dfb11c04174fdd0af2727fb75884b6fe3a42
TimMacrae <tim@getarthy.com>
2024-09-22 15:52:05 +0200

    add movie details item

--- apps/frontend/src/components/movies/movie-item.component.tsx
+++ apps/frontend/src/components/movies/movie-item.component.tsx
@@ -9,20 +9,31 @@ interface MovieItemProps {
 }
 
 export const MovieItem: React.FC<MovieItemProps> = ({ movie }) => {
-  const { release_date, poster, title, genre, averageRating } = movie;
+  const {
+    release_date,
+    poster,
+    title,
+    duration,
+    ratings,
+    commentsTotal,
+    averageRating,
+  } = movie;
   const releaseYear = new Date(release_date).getFullYear();
 
   return (
-    <div className="relative w-full h-full overflow-hidden rounded-lg shadow-lg">
+    <div className="relative w-full h-full overflow-hidden rounded-xl shadow-lg">
       <Image src={poster} alt={title} layout="fill" objectFit="cover" />
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-4">
         <h1 className="text-white text-xl font-bold">{title}</h1>
-        <p className="text-white">{genre}</p>
-        <p className="text-white">{releaseYear}</p>
+        <p className="text-white">{`Release year:  ${releaseYear}`}</p>
+        <p className="text-white">{`Duration: ${duration} min.`}</p>
+
         <p className="text-white flex items-center">
-          {averageRating}
-          <Star size={15} style={{ marginLeft: 4, marginTop: -2 }} />
+          {`Avarage rating: ${averageRating}`}
+          <Star size={15} className="text-yellow-400 ml-1" />
         </p>
+        <p className="text-white flex items-center">{`Ratings:${ratings}`}</p>
+        <p className="text-white flex items-center">{`Comments: ${commentsTotal}`}</p>
       </div>
     </div>
   );
