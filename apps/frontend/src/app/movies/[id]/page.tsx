db76dfe58fdd04fd3c4dcdebfada9572c29a1aa7
TimMacrae <tim@getarthy.com>
2024-09-23 17:26:58 +0200

    add dialog type

--- apps/frontend/src/app/movies/[id]/page.tsx
+++ apps/frontend/src/app/movies/[id]/page.tsx
@@ -6,6 +6,8 @@ import { MovieDetailItem } from "@/src/components/movies/movie-detail-item.compo
 import { useMovies } from "@/src/hooks/useMovies.query";
 import { Star } from "lucide-react";
 import Image from "next/image";
+import { APIROUTES } from "@/src/api/api-routes.config";
+import { GalleryMoviesDialog } from "@/src/components/gallery/gallery-movie-dialog.component";
 
 interface MoviePageProps {
   params: {
@@ -45,12 +47,15 @@ const MoviePage: React.FC<MoviePageProps> = ({ params }) => {
 
   return (
     <>
-      <Card className="m-4 p-4">
+      <Card className="m-4 p-4 relative">
+        <div className="absolute top-4 right-4">
+          {<GalleryMoviesDialog type="edit" movie={movie} />}
+        </div>
         <div className="container">
           <div className="flex flex-col md:flex-row">
             <div className="md:w-auto">
               <Image
-                src={movie.poster}
+                src={`${APIROUTES.API.ENDPOINT}${movie.poster}`}
                 alt={movie.title}
                 width={400}
                 height={650}
