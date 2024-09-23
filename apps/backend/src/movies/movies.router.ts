a5d62ec3c99ec05ae5cb266f4b7de523dbcfdfd2
TimMacrae <tim@getarthy.com>
2024-09-23 17:25:55 +0200

    cleanup

--- apps/backend/src/movies/movies.router.ts
+++ apps/backend/src/movies/movies.router.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import MovieController from "./movies.controller";
+import { authProtectionMiddleware } from "apps/backend/middleware/auth-protection.middleware";
 
 const router = express.Router();
 
