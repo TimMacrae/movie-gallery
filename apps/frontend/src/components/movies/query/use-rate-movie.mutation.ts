7bed23228ad69c47ccd88d7ca821a64763fb5939
TimMacrae <tim@getarthy.com>
2024-09-24 15:05:39 +0200

    add rate-movie-mutation

---
+++ apps/frontend/src/components/movies/query/use-rate-movie.mutation.ts
@@ -0,0 +1,45 @@
+"use client";
+import { APIROUTES } from "@/src/api/api-routes.config";
+import axios from "axios";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useToast } from "@/hooks/use-toast";
+import { Rating } from "@/src/types/movie.type";
+
+axios.defaults.withCredentials = true;
+
+interface RateMovieValues {
+  movie_id: string;
+  rating: number;
+}
+
+const postRateMovie = async (values: RateMovieValues): Promise<Rating> => {
+  const { data } = await axios.post<Rating>(
+    `${APIROUTES.API.ENDPOINT}${APIROUTES.API.POST_RATING}`,
+    values
+  );
+  return data;
+};
+
+export const useRateMovieMutation = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  return useMutation({
+    mutationFn: postRateMovie,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [APIROUTES.QUERY_KEYS.RATING],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [APIROUTES.QUERY_KEYS.MOVIES],
+      });
+    },
+    onError: (error) => {
+      toast({
+        variant: "destructive",
+        title: "Error, could not rate the movie",
+        description: error?.message || "Something went wrong.",
+      });
+    },
+  });
+};
