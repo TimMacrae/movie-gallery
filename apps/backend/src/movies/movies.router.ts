9d123689c2d75b6d685fe93bb0df2dc98a4afd6b
TimMacrae <tim@getarthy.com>
2024-09-24 11:37:12 +0200

    add route /movies/favorit

--- apps/backend/src/movies/movies.router.ts
+++ apps/backend/src/movies/movies.router.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import MovieController from "./movies.controller";
-import { authProtectionMiddleware } from "apps/backend/middleware/auth-protection.middleware";
+import { authProtectionMiddleware } from "../../middleware/auth-protection.middleware";
 
 const router = express.Router();
 
 router.get("/", MovieController.getMovies);
+router.post(
+  "/favorit",
+  authProtectionMiddleware,
+  MovieController.addToFavoritMovies
+);
 
 export default router;
