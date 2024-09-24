9970c0ce3a1b1c0602abe7a9eb0972f883d8896d
TimMacrae <tim@getarthy.com>
2024-09-24 15:04:14 +0200

    add new methods getRating rateMovie

--- apps/backend/src/movies/movies.controller.ts
+++ apps/backend/src/movies/movies.controller.ts
@@ -1,23 +1,12 @@
 import { Request, Response } from "express";
 import Movie from "../../model/movie-schema.model";
-import User from "../../model/user-schema.model";
+import Rating from "../../model/rating-schema.model";
 import { yearToDate } from "../../helper/year-to-date.helper";
 import { MoviesQueryParams } from "../../types/movie.type";
 import { BaseController } from "../utils/base.controller";
 import { IUser } from "apps/backend/types/user.type";
 
 class MovieController extends BaseController {
-  getMovieRatings(
-    arg0: string,
-    authProtectionMiddleware: (
-      req: Request,
-      res: Response,
-      next: import("express").NextFunction
-    ) => Promise<Response<any, Record<string, any>> | undefined>,
-    getMovieRatings: any
-  ) {
-    throw new Error("Method not implemented.");
-  }
   public getMovies = this.handleRequest(async (req: Request, res: Response) => {
     const {
       page = 1,
@@ -98,6 +87,49 @@ class MovieController extends BaseController {
       }
     }
   );
+
+  public getMovieRating = this.handleRequest(
+    async (req: Request, res: Response) => {
+      const { movie_id } = req.query;
+      const movie = await Movie.findById(movie_id);
+
+      if (!movie) {
+        throw new Error("Movie not found");
+      }
+
+      const rating = await Rating.findOne({ movie_id, user_id: req.user?._id });
+
+      res.status(200).json(rating);
+    }
+  );
+
+  public rateMovie = this.handleRequest(async (req: Request, res: Response) => {
+    const { movie_id, rating } = req.body;
+    const user = req.user as IUser;
+
+    const movie = await Movie.findById(movie_id);
+    if (!movie) {
+      throw new Error("Movie not found");
+    }
+
+    const existingRating = await Rating.findOne({
+      movie_id,
+      user_id: user._id,
+    });
+
+    if (existingRating) {
+      existingRating.rating = rating;
+      await existingRating.save();
+    } else {
+      await Rating.create({
+        movie_id,
+        user_id: user._id,
+        rating,
+      });
+    }
+
+    res.status(200).json({ message: "Rating added" });
+  });
 }
 
 export default new MovieController();
